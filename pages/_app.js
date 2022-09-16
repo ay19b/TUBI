30909da6d624571b4f74b5bcb5521caca70aa36d
ay19b <benmerabetayoub95@gmail.com>
2022-09-16 13:00:14 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,8 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import '../styles/globals.css'
-import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
-import theme from './theme';
 import createEmotionCache from '../component/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -19,11 +17,11 @@ export default function MyApp(props) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
+      <>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
-      </ThemeProvider>
+      </>
     </CacheProvider>
   );
 }
